d2020d5844bfea95d09d027d85ea9535ade7660b
Андрей Цепков <cepkov78@mail.ru>
2026-01-27 17:21:37 +0600

    delete string about static_root to remove error message, add "if not" statement

--- store/settings.py
+++ store/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT: str = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = ''
+    
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )

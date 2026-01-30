d8fd70b277f9bb18689faf4cb9b44871358013c8
Андрей Цепков <cepkov78@mail.ru>
2026-01-30 18:49:56 +0600

    add Product model to models.py

--- products/models.py
+++ products/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)

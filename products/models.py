8a969ea5c1a9773d4c0a0ba0b134d5508486b7f9
Андрей Цепков <cepkov78@mail.ru>
2026-01-30 18:38:46 +0600

    make name field of ProductCategory unique

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)

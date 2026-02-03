626c36643f2c4e398ff6b5278cb8c056cbde3ebf
Андрей Цепков <cepkov78@mail.ru>
2026-02-03 10:40:51 +0600

    override str method for product model

--- products/models.py
+++ products/models.py
@@ -14,3 +14,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'

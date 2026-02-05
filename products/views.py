4fdbd0bdcc659e44fe92d4113321e68c47a7ce09
Андрей Цепков <cepkov78@mail.ru>
2026-02-05 19:09:40 +0600

    import product and prod category from models to views.py and call products in context of products controller

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from products.models import ProductCategory, Product
+
 def index(request):
     context = {
         'title': 'Store', 
@@ -10,24 +12,6 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - Каталог',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.webp',
-                'name': 'Чёрный худи Adidas с монограммой',
-                'price': 6000,
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт для повседневной носки.'
-            },
-            {
-                'image': '/static/vendor/img/products/north_face.jpg',
-                'name': 'Синяя куртка The North Face',
-                'price': 23725,
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Лёгкий и тёплый пуховый наполнитель.'
-            },{
-                'image': '/static/vendor/img/products/asos_design.jpeg',
-                'name': 'Коричневый оверсайз-топ Asos Design',
-                'price': 3400,
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'
-            },
-        ]
+        'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context)

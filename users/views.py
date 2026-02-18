f606bfe0186b398f018062abd3be3de21bb59bcf
Андрей Цепков <cepkov78@mail.ru>
2026-02-18 21:24:05 +0600

    add files=request.FILES to UserProfileForm instance in profile handler

--- users/views.py
+++ users/views.py
@@ -33,7 +33,7 @@ def registration(request):
 
 def profile(request):
     if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST)
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('users:profile'))

a65cbcb9b5d067e9df3369694360cda0292571f6
Андрей Цепков <cepkov78@mail.ru>
2026-02-23 18:57:58 +0600

    implement basket_add handler

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 from products.models import ProductCategory, Product, Basket
 from users.models import User
@@ -19,4 +19,14 @@ def products(request):
     return render(request, 'products/products.html', context)
 
 def basket_add(request, product_id):
-    pass
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+    
+    return HttpResponseRedirect(request.META['HTTP_REFERRER'])

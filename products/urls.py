a4674af18982a3100e4e36e579db8d19ab21a2e9
Андрей Цепков <cepkov78@mail.ru>
2026-02-23 18:59:15 +0600

    add route for adding an item to basket in products/url.py

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from products.views import products
+from products.views import products, basket_add
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index')
+    path('', products, name='index'),
+    path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
 ]

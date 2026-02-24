c270ebab111008ca12a9676811b4c418174e0d4c
Андрей Цепков <cepkov78@mail.ru>
2026-02-24 09:55:35 +0600

    update profile handler to provide more data for user's basket

--- users/views.py
+++ users/views.py
@@ -44,10 +44,20 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
+    
+    baskets = Basket.objects.filter(user=request.user)
+    total_sum = 0
+    total_quantity = 0
+    for basket in baskets:
+        total_sum += basket.sum()
+        total_quantity += basket.quantity
+
     context = {
         'title': 'Store - Профиль пользователя', 
         'form': form,
-        'baskets': Basket.objects.filter(user=request.user),
+        'baskets': baskets,
+        'total_sum': total_sum,
+        'total_quantity': total_quantity,
     }
     return render(request, 'users/profile.html', context)
 

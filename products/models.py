31a5f5a9b505e032f41bd908660ebff698f1ee7d
Андрей Цепков <cepkov78@mail.ru>
2026-02-24 20:13:13 +0600

    create BasketQuerySet class extending queryset and move total sum and total quantity in it

--- products/models.py
+++ products/models.py
@@ -19,6 +19,13 @@ class Product(models.Model):
 
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+    
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
     
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
@@ -26,16 +33,10 @@ class Basket(models.Model):
     quantity = models.PositiveSmallIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
+    objects = BasketQuerySet.as_manager()
+
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
     
     def sum(self):
         return self.product.price * self.quantity
-    
-    def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user)
-        return sum(basket.sum() for basket in baskets)
-    
-    def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
-        return sum(basket.quantity for basket in baskets)

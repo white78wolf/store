03da58042744264374d3d2a40251625c3905549d
Андрей Цепков <cepkov78@mail.ru>
2026-02-25 18:22:11 +0600

    decorate basket_add and basket_remove in products/views.py with @login_required

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from products.models import ProductCategory, Product, Basket
 from users.models import User
@@ -18,6 +19,7 @@ def products(request):
     }
     return render(request, 'products/products.html', context)
 
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -31,6 +33,7 @@ def basket_add(request, product_id):
     
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()

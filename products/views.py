ee36a411cc27f257afa51ed68442b6a485ee0114
Андрей Цепков <cepkov78@mail.ru>
2026-03-02 10:36:01 +0600

    rewrite products handler to use it with category id

--- products/views.py
+++ products/views.py
@@ -11,11 +11,16 @@ def index(request):
     }
     return render(request, 'products/index.html', context)
 
-def products(request):
+def products(request, category_id):    
+    if category_id:
+        category = ProductCategory.objects.get(id=category_id)
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()
     context = {
-        'title': 'Store - Каталог',
-        'products': Product.objects.all(),
+        'title': 'Store - Каталог', 
         'categories': ProductCategory.objects.all(),
+        'products': products,
     }
     return render(request, 'products/products.html', context)
 

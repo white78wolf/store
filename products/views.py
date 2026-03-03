fe6f718be836ea166c2d22af0640cbaca8f08e10
Андрей Цепков <cepkov78@mail.ru>
2026-03-03 17:13:23 +0600

    extend  products handler to use paginator from django.core.paginator

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 from products.models import ProductCategory, Product, Basket
-from users.models import User
+from django.core.paginator import Paginator
 
 def index(request):
     context = {
@@ -11,11 +11,17 @@ def index(request):
     }
     return render(request, 'products/index.html', context)
 
-def products(request, category_id=None):    
+def products(request, category_id=None, page=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page)
+
     context = {
         'title': 'Store - Каталог', 
         'categories': ProductCategory.objects.all(), 
-        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+        'products': products_paginator,
     }
     return render(request, 'products/products.html', context)
 

a1c43d40d704e8f82f0412b791602aeecf01451e
Андрей Цепков <cepkov78@mail.ru>
2026-03-06 17:13:12 +0600

    fix path to display products by categories in products/urls.py url patterns

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),   
-    path('category/<int:category>/', ProductsListView.as_view(), name='category'), 
+    path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),     
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]

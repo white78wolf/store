227c970b306f5a261d8ba97cd6c9638dbdb4a941
Андрей Цепков <cepkov78@mail.ru>
2026-03-06 17:12:08 +0600

    override get_queryset in ProductsListView class to use filtering products due to categories

--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ from products.models import ProductCategory, Product, Basket
 from django.core.paginator import Paginator
 
 class IndexView(TemplateView):
-    template_name = 'products/index.html'
+    template_name = 'products/index.html'    
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data()
@@ -18,25 +18,16 @@ class ProductsListView(ListView):
     model = Product
     template_name = 'products/products.html'
 
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductsListView, self).get_context_data()
         context['title'] = 'Store - Каталог'
         context['categories'] = ProductCategory.objects.all()
         return context
-
-""" def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
-
-    context = {
-        'title': 'Store - Каталог', 
-        'categories': ProductCategory.objects.all(), 
-        'products': products_paginator,
-    }
-    return render(request, 'products/products.html', context) """
 
 @login_required
 def basket_add(request, product_id):

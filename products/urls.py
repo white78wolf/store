aedf3ec2399600225285298458d2707edf4cfb4a
Андрей Цепков <cepkov78@mail.ru>
2026-03-06 09:48:42 +0600

    restore url to categories in products/urls.py using ProductListView.as_view

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,8 @@ from products.views import ProductsListView, basket_add, basket_remove
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='index'),    
+    path('', ProductsListView.as_view(), name='index'),   
+    path('category/<int:category>/', ProductsListView.as_view(), name='category'), 
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]

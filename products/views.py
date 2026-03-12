4ccd43c2f8505cc7236f2c46d91999222a3d868b
Андрей Цепков <cepkov78@mail.ru>
2026-03-12 16:40:48 +0600

    import CreateView to products/views.py

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 from products.models import ProductCategory, Product, Basket
 from django.core.paginator import Paginator

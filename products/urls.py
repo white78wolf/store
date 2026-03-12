2686bad298f1d07330a61cbff83b99228a8bdb94
Андрей Цепков <cepkov78@mail.ru>
2026-03-12 15:20:41 +0600

    add url pattern to products/urls.py for paginating

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,8 @@ app_name = 'products'
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),   
-    path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),     
+    path('category/<int:category_id>/', ProductsListView.as_view(), name='category'), 
+    path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]

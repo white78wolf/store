97336d0b5039ae8c4a7fd2c5c78a7a5106381b60
Андрей Цепков <cepkov78@mail.ru>
2026-03-13 11:02:45 +0600

    change url pattern for registration route to using UserRegistrationView as view in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from users.views import login, registration, profile, logout
+from users.views import login, UserRegistrationView, profile, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration/', registration, name='registration'),
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout')
 ]

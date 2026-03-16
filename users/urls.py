61addb84c133f3ebe012e47e0743dc3875b46444
Андрей Цепков <cepkov78@mail.ru>
2026-03-16 19:42:44 +0600

    use UserProfileView as view in url patterns in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from users.views import login, UserRegistrationView, profile, logout
+from users.views import login, UserRegistrationView, UserProfileView, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
-    path('profile/', profile, name='profile'),
+    path('profile/<int:pk>', UserProfileView.as_view(), name='profile'),
     path('logout/', logout, name='logout')
 ]

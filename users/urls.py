a7cd6c966ffdddb3f35c84658c35a732d2c70d11
Андрей Цепков <cepkov78@mail.ru>
2026-03-16 19:53:43 +0600

    add login_required to users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from django.contrib.auth.decorators import login_required
 from users.views import login, UserRegistrationView, UserProfileView, logout
 
 app_name = 'users'
@@ -7,6 +8,6 @@ app_name = 'users'
 urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
-    path('profile/<int:pk>', UserProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', logout, name='logout')
 ]

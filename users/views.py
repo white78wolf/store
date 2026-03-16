5b3fe65f996e33e64088a24f4b95ca54c5218220
Андрей Цепков <cepkov78@mail.ru>
2026-03-16 19:53:10 +0600

    delete login_required decorator

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy
-from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView
 
 from users.models import User

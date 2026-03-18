5f43e9ead60d8e3856df29b612a40ee9041f2fd6
Андрей Цепков <cepkov78@mail.ru>
2026-03-18 09:52:45 +0600

    remove not used imports from products/views.py

--- products/views.py
+++ products/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
 from products.models import ProductCategory, Product, Basket
-from django.core.paginator import Paginator
 
 class IndexView(TemplateView):
     template_name = 'products/index.html'    

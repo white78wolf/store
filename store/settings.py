76d86843f7fba936e4061920f4e6af63f79f0ff6
Андрей Цепков <cepkov78@mail.ru>
2026-03-25 10:38:32 +0600

    add email_backend to store/settings.py

--- store/settings.py
+++ store/settings.py
@@ -141,3 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
+
+# Sending emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

06a2e00d4fd6b191a0f1f765a9f1f978bccf4a8e
Андрей Цепков <cepkov78@mail.ru>
2026-03-25 11:09:31 +0600

    create EmailVerification class for users/models.py

--- users/models.py
+++ users/models.py
@@ -4,3 +4,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
     
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'

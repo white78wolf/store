32394314efe9a3440e174739e75503bae2b45400
Андрей Цепков <cepkov78@mail.ru>
2026-04-02 10:44:53 +0600

    add path for email verification controller to users/views.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from django.contrib.auth.decorators import login_required
-from users.views import UserLoginView, UserRegistrationView, UserProfileView, logout
+from users.views import UserLoginView, UserRegistrationView, UserProfileView, EmailVerificationView, logout
 
 app_name = 'users'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
-    path('logout/', logout, name='logout')
+    path('logout/', logout, name='logout'),
+    path('verify/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name='email_verification')
 ]

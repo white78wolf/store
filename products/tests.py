27105ef5d35f19e14191a02434f5d82825b5e962
Андрей Цепков <cepkov78@mail.ru>
2026-04-20 22:00:46 +0600

    write test class in products/tests.py for IndexView class

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,10 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTestCase(TestCase):
+    
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+        print(response)

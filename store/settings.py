95dcda1c6cfa557e72f9de3de5465194b64f8b73
Андрей Цепков <cepkov78@mail.ru>
2026-04-20 10:58:33 +0600

    update settings.py to use django-allauth

--- store/settings.py
+++ store/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
 
     'products',
     'users',
@@ -158,3 +164,16 @@ LOGIN_REDIRECT_URL = '/'
 # Sending emails
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# OAuth
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+SOCIALACCOUNT_PROVIDERS = {
+    
+}

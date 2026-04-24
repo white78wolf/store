877ec30797467f72fe0e38bb9af18c351c39e3aa
Андрей Цепков <cepkov78@mail.ru>
2026-04-24 16:53:44 +0600

    collect all instances of Product in one varable for ProductListViewTestCase

--- products/tests.py
+++ products/tests.py
@@ -20,26 +20,27 @@ class IndexViewTestCase(TestCase):
 class ProductsListViewTestCase(TestCase):
     fixtures = ['categories.json', 'goods.json']
 
+    def setUp(self):
+        self.products = Product.objects.all()
+
     def test_list(self):
         path = reverse('products:index')
         response = self.client.get(path)
-
-        products = Product.objects.all()        
+        
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store - Каталог')
         self.assertTemplateUsed(response, 'products/products.html')
-        self.assertEqual(list(response.context_data['object_list']), list(products[:3]))
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[:3]))
     
     def test_list_with_category(self):
         category = ProductCategory.objects.first()
         path = reverse('products:category', kwargs={'category_id': category.id})
         response = self.client.get(path)
-
-        products = Product.objects.all()
+        
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store - Каталог')
         self.assertTemplateUsed(response, 'products/products.html')
         self.assertEqual(
             list(response.context_data['object_list']),
-            list(products.filter(category_id = category.id))
+            list(self.products.filter(category_id = category.id))
         )

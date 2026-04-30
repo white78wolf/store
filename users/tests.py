08b8ebac052e99cdddc12dc7df8ab41c68a440b0
Андрей Цепков <cepkov78@mail.ru>
2026-04-30 10:24:44 +0600

    write UserRegistrationViewTestCase for users/tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class UserRegistrationViewTestCase(TestCase):
+
+    def setUp(self):
+        self.path = reverse('users:registration')
+    
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Регистрация')

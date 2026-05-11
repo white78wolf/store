8a8ac81a7090dbb30dfdde2c10c95d3734ee2902
Андрей Цепков <cepkov78@mail.ru>
2026-05-11 17:04:58 +0600

    update test_user_registration_get method with checking on redirection and email verification

--- users/tests.py
+++ users/tests.py
@@ -1,15 +1,22 @@
+from datetime import timedelta
 from http import HTTPStatus
 
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.timezone import now
 
-from users.forms import UserRegistrationForm
+from users.models import User, EmailVerification
 
 
 class UserRegistrationViewTestCase(TestCase):
 
     def setUp(self):
         self.path = reverse('users:registration')
+        self.data = {
+            'first_name': 'Антон', 'last_name': 'Городецкий',
+            'username': 'Dark78Beer', 'email': 'example@mail.ru',
+            'password1': '12345678pP', 'password2': '12345678pP',            
+        }
     
     def test_user_registration_get(self):
         response = self.client.get(self.path)
@@ -17,3 +24,19 @@ class UserRegistrationViewTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data['title'], 'Store - Регистрация')
         self.assertTemplateUsed(response, 'users/registration.html')
+    
+    def test_user_registration_post_success(self):        
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)        
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )

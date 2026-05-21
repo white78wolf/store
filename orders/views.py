dae3802b9cba3fc24876a26c7ec1053709cdab8a
Андрей Цепков <cepkov78@mail.ru>
2026-05-21 09:41:34 +0600

    import templateview to orders/views.py and change createview to template view

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
-class OrderCreateView(CreateView):
+class OrderCreateView(TemplateView):
     template_name = 'orders/order-create.html'

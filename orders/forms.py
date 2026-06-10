ecac271ea071c127b3d99901000b5449704fb8ae
Андрей Цепков <cepkov78@mail.ru>
2026-06-10 19:09:31 +0600

    update OrderForm class in orders/forms.py

--- orders/forms.py
+++ orders/forms.py
@@ -4,7 +4,13 @@ from orders.models import Order
 
 
 class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'example@example.com'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Россия, Санкт-Петербург, пр. Невский, дом 7',
+    }))
+
     class Meta:
         model = Order
         fields = ('first_name', 'last_name', 'email', 'address')
-        
